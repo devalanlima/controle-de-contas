f034b7d68f990352894c5219bbff7e0edadb9a2f
Alan Lima <dev.alanlima@gmail.com>
2024-09-20 16:40:32 -0300

    feat: Create checkbox component

---
+++ src/renderer/components/Atoms/AtomsCheckbox.vue
@@ -0,0 +1,34 @@
+<template>
+      <label class="cursor-pointer">
+        <div 
+          class="w-3 h-3 transition-all ease-in-out duration-150 ring-2 border-2 border-white ring-transparent outline-2 outline outline-gray-800"
+          :class="inputCheckbox ? 'bg-gray-800' : 'bg-white'"
+        >
+        </div>
+      <input class="sr-only" type="checkbox" v-model="inputCheckbox" />
+    </label>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  modelValue: boolean
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits<{
+  'update:modelValue': [value: typeof props.modelValue]
+}>()
+
+const inputCheckbox = computed({
+  get() {
+    return props.modelValue
+  },
+  set(inputCheckbox) {
+    emit('update:modelValue', inputCheckbox)
+  }
+})
+
+</script>
